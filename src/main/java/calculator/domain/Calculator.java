9b91e7db29a576c04e4d4ed50b8d6ec327b2186d
xodbs126 <dltvmsskd@naver.com>
2025-10-14 20:59:48 +0900

    feat(Calculator): SplitNumber 클래스 주입 및 결과 계산 로직

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,4 +1,17 @@
 package calculator.domain;
 
 public class Calculator {
+
+    private final SplitNumber splitNumber;
+
+    public int add(String stringInputs) {
+        int[] splitNum = splitNumber.splitNum(stringInputs);
+        int sum = 0;
+
+        for (int num : splitNum) {
+            sum += num;
+        }
+        return sum;
+    }
+
 }
