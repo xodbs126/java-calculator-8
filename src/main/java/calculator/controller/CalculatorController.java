35302437ff9b985f2bd8912f5881eedfd7283365
xodbs126 <dltvmsskd@naver.com>
2025-10-14 18:40:23 +0900

    feat(CalculatorController): CalculatorController 설계

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,5 +1,25 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
+import calculator.view.InputView;
+import calculator.view.ResultView;
+
 public class CalculatorController {
+
+    private final InputView inputView;
+    private final ResultView resultView;
+    private final Calculator calculator;
+
+    public CalculatorController(InputView inputView, ResultView resultView, Calculator calculator) {
+        this.inputView = inputView;
+        this.resultView = resultView;
+        this.calculator = calculator;
+    }
+
+    public void runCalculator() {
+        String stringInputs = inputView.input();
+        int result = calculator.add(stringInputs);
+        resultView.printResult(result);
+    }
 
 }
