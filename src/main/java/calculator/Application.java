2612f1cd483bf40f24357f54c6064e0dc4b86ae9
xodbs126 <dltvmsskd@naver.com>
2025-10-15 15:08:58 +0900

    feat(app): 애플리케이션 의존성 주입

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.Calculator;
+import calculator.domain.NumberSeparator;
+import calculator.domain.SeparatorGenerator;
+import calculator.view.InputView;
+import calculator.view.ResultView;
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+
+    private final CalculatorController controller;
+
+    public Application(CalculatorController controller) {
+        this.controller = controller;
+    }
+
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
+        SeparatorGenerator separatorGenerator = new SeparatorGenerator();
+        NumberSeparator numberSplitter = new NumberSeparator(separatorGenerator);
+        Calculator calculator = new Calculator(numberSplitter);
+
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        
+        CalculatorController controller = new CalculatorController(inputView, resultView, calculator);
+
+        controller.runCalculator();
     }
 }
