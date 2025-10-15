95af42e32b20b52c3199f3eb2a121f0694dbe367
xodbs126 <dltvmsskd@naver.com>
2025-10-15 15:29:10 +0900

    test: 사용자 입력에 따른 테스트 코드

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,12 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,13 +20,51 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+
+    }
+
+    @Test
+    void 음수_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n1;-2;3"))
+                        .isInstanceOf(IllegalArgumentException.class));
+
+    }
+
+    @Test
+    void 숫자_외_잘못된_입력() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//-\\n1-2qwe--3"))
+                        .isInstanceOf(IllegalArgumentException.class));
+
+    }
+
+    @Test
+    void 연속된_구분자_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//-\\n1-2--3"))
+                        .isInstanceOf(IllegalArgumentException.class));
+
+    }
+
+
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        try {
+            Application.main(new String[]{});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
