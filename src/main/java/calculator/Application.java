c61385523c490c4554f02f6e233fcadd28d6dea8
xodbs126 <dltvmsskd@naver.com>
2025-10-18 03:58:46 +0900

    refactor(app): 애플리케이션 의존성 주입 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,8 +2,11 @@ package calculator;
 
 import calculator.controller.CalculatorController;
 import calculator.domain.Calculator;
-import calculator.domain.NumberSeparator;
 import calculator.domain.SeparatorGenerator;
+import calculator.domain.parser.NumberParser;
+import calculator.domain.separator.StringSeparator;
+import calculator.domain.validator.NumberRangeValidator;
+import calculator.domain.validator.SeparatorValidator;
 import calculator.view.InputView;
 import calculator.view.ResultView;
 import java.io.IOException;
@@ -13,11 +16,14 @@ public class Application {
     public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
         SeparatorGenerator separatorGenerator = new SeparatorGenerator();
-        NumberSeparator numberSplitter = new NumberSeparator(separatorGenerator);
-        Calculator calculator = new Calculator(numberSplitter);
-
+        SeparatorValidator separatorValidator = new SeparatorValidator();
+        NumberRangeValidator numberRangeValidator = new NumberRangeValidator();
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
+
+        StringSeparator stringSeparator = new StringSeparator(separatorGenerator, separatorValidator);
+        NumberParser numberParser = new NumberParser(numberRangeValidator, stringSeparator);
+        Calculator calculator = new Calculator(numberParser);
 
         CalculatorController controller = new CalculatorController(inputView, resultView, calculator);
 
