36ef5327e74fec0a78f0c241c4a0361240e99226
xodbs126 <dltvmsskd@naver.com>
2025-10-19 00:58:36 +0900

    refactor(app): 구분자 전략 의존성 주입

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,26 +2,30 @@ package calculator;
 
 import calculator.controller.CalculatorController;
 import calculator.domain.Calculator;
-import calculator.domain.SeparatorGenerator;
 import calculator.domain.parser.NumberParser;
 import calculator.domain.separator.StringSeparator;
+import calculator.domain.separator.strategy.CustomPatternSeparator;
+import calculator.domain.separator.strategy.DefaultSeparator;
+import calculator.domain.separator.strategy.Separator;
 import calculator.domain.validator.NumberRangeValidator;
 import calculator.domain.validator.SeparatorValidator;
 import calculator.view.InputView;
 import calculator.view.ResultView;
 import java.io.IOException;
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
-        SeparatorGenerator separatorGenerator = new SeparatorGenerator();
+        List<Separator> separators = List.of(new CustomPatternSeparator(), new DefaultSeparator());
+
         SeparatorValidator separatorValidator = new SeparatorValidator();
         NumberRangeValidator numberRangeValidator = new NumberRangeValidator();
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
-        StringSeparator stringSeparator = new StringSeparator(separatorGenerator, separatorValidator);
+        StringSeparator stringSeparator = new StringSeparator(separators, separatorValidator);
         NumberParser numberParser = new NumberParser(numberRangeValidator, stringSeparator);
         Calculator calculator = new Calculator(numberParser);
 
