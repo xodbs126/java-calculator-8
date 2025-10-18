bdb0dabd2bbef3d19736be87136e42c6fbfe7e6e
xodbs126 <dltvmsskd@naver.com>
2025-10-19 00:58:29 +0900

    refactor(StringSeparator): 구분자 전략 의존성 주입

--- src/main/java/calculator/domain/separator/StringSeparator.java
+++ src/main/java/calculator/domain/separator/StringSeparator.java
@@ -1,25 +1,36 @@
 package calculator.domain.separator;
 
-import calculator.domain.SeparatorGenerator;
+import calculator.domain.separator.strategy.Separator;
 import calculator.domain.validator.Validator;
 import calculator.dto.SplitStringDto;
+import java.util.List;
+import java.util.Optional;
 
 public class StringSeparator {
 
-    private final SeparatorGenerator separatorGenerator;
+    private final List<Separator> separators;
     private final Validator<String> validator;
 
-    public StringSeparator(SeparatorGenerator separatorGenerator, Validator validator) {
-        this.separatorGenerator = separatorGenerator;
+    public StringSeparator(List<Separator> separators, Validator<String> validator) {
+        this.separators = separators;
         this.validator = validator;
     }
 
     public String[] separate(String inputs) {
-        SplitStringDto separatorAndTarget = separatorGenerator.separateString(inputs);
+        SplitStringDto separatorAndTarget = findFirstApplicableSeparator(inputs);
+
         String separator = separatorAndTarget.separator();
         validator.isValid(separator);
         String targetString = separatorAndTarget.targetString();
-
         return targetString.split(separator);
+    }
+
+    private SplitStringDto findFirstApplicableSeparator(String inputs) {
+        return separators.stream()
+                .map(separator -> separator.separateString(inputs))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("구분자 전략을 찾을 수 없습니다."));
     }
 }
