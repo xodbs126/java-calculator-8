a90d4e5ba12a894baaed5b1da275f39eaab031d5
xodbs126 <dltvmsskd@naver.com>
2025-10-20 20:40:34 +0900

    refactor(app): IOException 제거 및 주석 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,25 +10,36 @@ import calculator.domain.separator.strategy.Separator;
 import calculator.domain.validator.NumberRangeValidator;
 import calculator.view.InputView;
 import calculator.view.ResultView;
-import java.io.IOException;
 import java.util.List;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // TODO: 프로그램 구현
-        List<Separator> separators = List.of(new CustomPatternSeparator(), new DefaultSeparator());
 
+        List<Separator> separators = List.of(new CustomPatternSeparator(),
+                new DefaultSeparator()); // 구분자를 정하는 strategy 생성
+
+        /***
+         * 입력값에 대한 유효성 검사 로직 생성
+         */
         NumberRangeValidator numberRangeValidator = new NumberRangeValidator();
+
+        /***
+         * 입출력 View 생성
+         */
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
+        /***
+         * 각 객체에 대한 의존성 주입
+         */
         StringSeparator stringSeparator = new StringSeparator(separators);
         NumberParser numberParser = new NumberParser(numberRangeValidator, stringSeparator);
         Calculator calculator = new Calculator(numberParser);
 
         CalculatorController controller = new CalculatorController(inputView, resultView, calculator);
 
-        controller.runCalculator();
+        controller.runCalculator(); // 덧셈 계산기 실행
     }
 }
